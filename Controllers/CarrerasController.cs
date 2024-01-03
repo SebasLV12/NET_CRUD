d24bdf3d7f9ae42292dd9c41a4c32fcf45b8eca7
Sebastian Lezama <Sebastian.Lezama@pe.ey.com>
2024-01-03 14:55:48 -0500

    Prueba tecnica Prayaga

---
+++ Controllers/CarrerasController.cs
@@ -0,0 +1,115 @@
+﻿using Crud_prayaga.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_prayaga.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        private readonly DbModels context;
+
+        public CarrerasController(DbModels dbContext)
+        {
+            context = dbContext;
+        }
+
+        public ActionResult Index()
+        {
+        
+                return View(context.carrera.ToList());
+
+        }
+
+        // GET: carrera/Details/5
+        public ActionResult Details(int id)
+        {
+
+                return View(context.carrera.Where(x => x.id == id));
+  
+        }
+
+        // POST: carrera/Create
+        [HttpPost]
+        public ActionResult Create(carrera carrera)
+        {
+            try
+            {
+                
+                context.carrera.Add(carrera);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+
+        // GET: carrera/Edit/5
+        public ActionResult Edit(int id)
+        {
+            
+                return View(context.carrera.Where(x => x.id == id).FirstOrDefault());
+       
+        }
+
+        // POST: carrera/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, carrera carrera)
+        {
+            try
+            {
+                context.Entry(carrera).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+
+        // GET: carrera/Delete/5
+        public ActionResult Delete(int id)
+        {
+            
+                return View(context.carrera.Where(x => x.id == id).FirstOrDefault());
+            
+        }
+
+        // POST: carrera/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, carrera carrera)
+        {
+            try
+            {
+
+                carrera carreraToRemove = context.carrera.SingleOrDefault(x => x.id == id && !x.eliminado);
+                if (carreraToRemove == null)
+                {
+                    TempData["ErrorMessage"] = "La carrera que intentas eliminar no existe o ya ha sido eliminada.";
+                    return RedirectToAction("Index");
+                }
+                carreraToRemove.eliminado = true;
+                context.SaveChanges();
+                TempData["SuccessMessage"] = "La carrera se eliminó correctamente.";
+                return RedirectToAction("Index");   
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+        public ActionResult GetDeleted()
+        {
+            
+                var deletedCarreras = context.carrera.Where(x => x.eliminado).ToList();
+                return View(deletedCarreras);
+            
+        }
+    }
+}
---
+++ Controllers/FacultadController.cs
@@ -0,0 +1,145 @@
+﻿using Crud_prayaga.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Crud_prayaga.Controllers
+{
+    public class FacultadController : Controller
+    {
+
+        private readonly DbModels context;
+
+        public FacultadController(DbModels dbContext)
+        {
+            context = dbContext;
+        }
+
+        public ActionResult Index()
+        {
+
+                return View(context.facultad.ToList());
+
+        }
+
+        // GET: Facultad/Details/5
+        public ActionResult Details(int id)
+        {
+
+            
+
+                return View(context.facultad.Where(x=>x.id==id));
+            
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Facultad/Create
+        [HttpPost]
+        public ActionResult Create(facultad facultad)
+        {
+            try
+            {
+                
+                    context.facultad.Add(facultad);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Facultad/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            return View(context.facultad.Where(x => x.id == id).FirstOrDefault());
+        }
+
+        // POST: Facultad/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, facultad facultad)
+        {
+            try { 
+             
+                    context.Entry(facultad).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+
+        // GET: Facultad/Delete/5
+        public ActionResult Delete(int id)
+        {
+         
+                return View(context.facultad.Where(x => x.id == id).FirstOrDefault());
+         
+        }
+
+        // POST: Facultad/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, int? nuevaFacultadId)
+        {
+            try
+            {
+               
+                    var facultadRemoved = context.facultad.SingleOrDefault(x => x.id == id && !x.eliminado);
+
+                    if (facultadRemoved == null)
+                    {
+                        TempData["ErrorMessage"] = "La carrera no existe o ya ha sido eliminada.";
+
+                        return RedirectToAction("Index");
+                    }
+
+                if (nuevaFacultadId.HasValue)
+                    {
+                        var nuevaFacultad = context.facultad.Find(nuevaFacultadId.Value);
+                        if (nuevaFacultad == null || nuevaFacultad.eliminado)
+                        {
+                            TempData["ErrorMessage"] = "La nueva facultad no existe o ya ha sido eliminada."
+                            return RedirectToAction("Index");
+                        }
+
+                        foreach (var carrera in facultadRemoved.carrera)
+                        {
+                            carrera.facultad = nuevaFacultadId.Value;
+                        }
+                    }
+
+                    facultadRemoved.eliminado = true;
+                    context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+           
+        }
+
+
+        public ActionResult GetDeleted()
+        {
+           
+
+                var deletedFacultades = context.facultad.Where(x => x.eliminado).ToList();
+                return View(deletedFacultades);
+            
+        }
+    }
+}
